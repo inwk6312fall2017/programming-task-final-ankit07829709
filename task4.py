c7471b9a21d7429a40f997c8b6a6809ef48ea0fe
ankit07829709 <Ankit.sharma@dal.ca>
2017-10-20 18:42:46 +0000

    changes

--- task4.py
+++ task4.py
@@ -1,5 +1,10 @@
 import create_ticket as ticketgen
 import requests
+
  def ticketcount():
+
   ticket = ticketgen.get_ticket()
-    api = "/api/v1/network-device/count"
+  api = "/api/v1/network-device/count"
+  controller = 'sandboxapic.cisco.com'
+  api = "/api2/v2/network-device/count"
+  url = "https://" + controller + api
