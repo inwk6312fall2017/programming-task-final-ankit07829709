c05a39016751342c0a974c2c34d058b1c1c245f8
ankit07829709 <Ankit.sharma@dal.ca>
2017-10-20 18:55:01 +0000

    editing

--- task4.py
+++ task4.py
@@ -8,3 +8,5 @@ import requests
   controller = 'sandboxapic.cisco.com'
   api = "/api2/v2/network-device/count"
   url = "https://" + controller + api
+  header_content = {"content-type": "application/json", "X-Auth-Token": ticket}
+    response_val = requests.get(url, headers=header_content, verify=False)
